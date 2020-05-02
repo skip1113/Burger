b7fbe3ba92af2caca3bb223dd7b7b15ed8077add
Skip Khamvongsa <skipk95@gmail.com>
2020-05-02 15:35:58 -0600

    Added folders and files for app, functions for routes, and data

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+var router = express.Router();
+
+//Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ],[
+        req.body.name, req.body.devoure
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("Devoured? ", condition);
+    burger.updateOne({
+        devoured: req.body.sleepy
+    }, condition, function(result) {
+        if(results.changeRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    
+    });
+});
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+//Importing orm to create functions that will interact with the DB.
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.create("burgers", col, val, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+//Export database functions for controller
+module.exports = burger;
