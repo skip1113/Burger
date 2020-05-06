6d9a857ab121ca1f3a5882fd0c2d9d9cdbd2bf8f
Skip Khamvongsa <skipk95@gmail.com>
2020-05-06 17:51:08 -0600

    Jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'laos1038863',
     database: 'burgers_db'
   });
 };
